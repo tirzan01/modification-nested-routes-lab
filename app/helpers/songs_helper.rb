1ad0ce794b8c1936b9a6bf6e332881680c957d9d
Thomas Montalto <thomasmonta98@gmail.com>
2021-12-02 16:53:10 +1100

    done with assignment

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def artist_id_field(song)
+    # if song.artist.nil?
+      select_tag :song_artist_id, options_from_collection_for_select(Artist.all, :id, :name), :include_blank => true
+    # else
+    #   puts song.artist.name, '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    #   hidden_field_tag 'song_artist_id', song.artist_id
+    # end
+  end
 end
